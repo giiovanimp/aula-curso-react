ac4af7d795d81bcdc70cd15dfef5ab5d5ae509e5
Giovani Martins Pires <giovani.pires@castgroup.com.br>
2019-06-13 19:38:08 -0300

    Apresentando alunos cadastrados

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Alert, TouchableHighlight, TouchableOpacity } from "react-native";
+import { View, Text, Alert, ActivityIndicator, TouchableOpacity } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Button } from "native-base";
 
@@ -17,32 +17,47 @@ class HomeScreen extends React.Component {
   };
 
   state = {
-    cursos: []
+    cursos: [],
+    isLoading: true
   }
 
   componentDidMount = async () => {
-    const urlCursos = 'http://104.248.133.2:7001/cursos';
-    const response = await fetch(urlCursos);
+    const urlCursoDetalhado = 'http://104.248.133.2:7001/cursos';
+    const response = await fetch(urlCursoDetalhado);
     const responseParsead = await response.json();
 
-    this.setState({ cursos: responseParsead });
+    this.setState({ cursos: responseParsead, isLoading: false });
 
   }
 
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          padding: 10
+        }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )
+    }
+
     return (
       <View style={{ flex: 1 }}>
         {this.state.cursos.map(curso => {
           return (
             <TouchableOpacity
-              onPress={() => Alert.alert(`Clicamos no curso de index ${curso.id} que é o ${curso.nome}`)}
+              onPress={() => this.props.navigation.navigate('Second', { id: curso.id })}
               key={curso.nome}
               style={{
                 flexDirection: 'row',
                 padding: 15,
                 borderBottomColor: '#ccc',
                 borderBottomWidth: 1,
-                // justifyContent: 'center',
                 alignItems: 'center'
               }}>
               <Text style={{ marginLeft: 15, fontSize: 22 }}> {curso.id}</Text>
@@ -58,9 +73,32 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  
+  state = {
+    cursoDetalhado: [],
+    alunosCadastrados: [],
+    isLoading: true
+  }
+
+  componentDidMount = async () => {
+    const { navigation } = this.props;
+    const idCurso = navigation.getParam('id');
+
+    const urlCursoDetalhado = 'http://104.248.133.2:7001/cursos/' + idCurso;
+    const response = await fetch(urlCursoDetalhado);
+    const responseParsead = await response.json();
+
+    const urlAlunosInscritos = 'http://104.248.133.2:7001/cursos/' + idCurso + '/alunos-inscritos';
+    const responseAlunos = await fetch(urlAlunosInscritos);
+    const responseAlunosParsead = await responseAlunos.json();
+
+    this.setState({ cursoDetalhado: responseParsead[0],
+      alunosCadastrados: responseAlunosParsead,
+      isLoading: false });
+  }
 
   static navigationOptions = {
-    title: 'Second',
+    title: 'Detalhes',
     headerStyle: {
       backgroundColor: 'red',
     },
@@ -71,20 +109,53 @@ class DetailsScreen extends React.Component {
   };
 
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          padding: 10
+        }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )
+    }
+
     return (
-      <View style={{ flex: 1 }}>
-        <Text>
-          {this.state.cursos.map(curso => {
-            return (
-              <View key={curso.id}>
-                <Text>{curso.id}</Text>
-                <Text>{curso.nome}</Text>
-              </View>
-            )
-          })}
-        </Text>
-        <Text>{this.state.cursos.length > 0 && this.state.cursos[0].id}</Text>
-        <Text>{this.state.cursos.length > 0 && this.state.cursos[0].nome}</Text>
+      <View style={{ flex: 1, backgroundColor: '#eee',  padding: 20}}>
+        <View style={{ backgroundColor: '#fff', borderColor: '#ccc', borderWidth: 1, borderRadius: 10, padding: 12 }}>
+          <Text>Id: {this.state.cursoDetalhado.id}</Text>
+          <Text>Curso: {this.state.cursoDetalhado.nome}</Text>
+          <Text>Professor: {this.state.cursoDetalhado.professor}</Text>
+          <Text>Local: {this.state.cursoDetalhado.local}</Text>
+        </View>
+
+        <View style={{ marginTop: 10, backgroundColor: '#fff', borderColor: '#ccc', borderWidth: 1, borderRadius: 10, padding: 12 }}>
+          <View style={{backgroundColor: '#bbb', alignItems: 'center', justifyContent: 'center', padding: 15, borderRadius: 10}}>
+            <Text>Alunos Cadastrados</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            {this.state.alunosCadastrados.map(aluno => {
+              return (
+                <View
+                  key={aluno.nome}
+                  style={{
+                    flexDirection: 'row',
+                    padding: 15,
+                    borderBottomColor: '#ccc',
+                    borderBottomWidth: 1,
+                    alignItems: 'center'
+                  }}>
+                  <Text style={{ marginLeft: 15, fontSize: 22 }}> {aluno.id}</Text>
+                  <Text style={{ marginLeft: 15, fontSize: 22 }}>{aluno.nome}</Text>
+                </View>
+              )
+            })}
+          </View>
+        </View>
       </View>
     )
   }
