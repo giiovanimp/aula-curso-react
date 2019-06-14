f8749e5a335633786929ee4ca6700eb636421448
Giovani Martins Pires <giovani.pires@castgroup.com.br>
2019-06-14 18:09:37 -0300

    Separa styles

--- components/detailsScreen.js
+++ components/detailsScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
+import styles from './styles/styles';
 
 export default class DetailsScreen extends Component {
 
@@ -86,41 +87,3 @@ export default class DetailsScreen extends Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: '#eee',
-    padding: 20
-  },
-  infoBox: {
-    backgroundColor: '#fff',
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 10,
-    padding: 12
-  },
-  studentsBox: {
-    backgroundColor: '#fff',
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 10,
-    marginTop: 10,
-    padding: 12
-  },
-  titleBox: {
-    backgroundColor: '#bbb',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-    borderRadius: 10,
-    margin: 5
-  },
-  studentsDesription: {
-    marginLeft: 15,
-    fontSize: 16
-  },
-  studentsLine: {
-    flexDirection: 'row'
-  }
-});
---
+++ components/styles/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    main: {
+      flex: 1,
+      backgroundColor: '#eee',
+      padding: 20
+    },
+    infoBox: {
+      backgroundColor: '#fff',
+      borderColor: '#ccc',
+      borderWidth: 1,
+      borderRadius: 10,
+      padding: 12
+    },
+    studentsBox: {
+      backgroundColor: '#fff',
+      borderColor: '#ccc',
+      borderWidth: 1,
+      borderRadius: 10,
+      marginTop: 10,
+      padding: 12
+    },
+    titleBox: {
+      backgroundColor: '#bbb',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      borderRadius: 10,
+      margin: 5
+    },
+    studentsDesription: {
+      marginLeft: 15,
+      fontSize: 16
+    },
+    studentsLine: {
+      flexDirection: 'row'
+    }
+  });
+
+  export default styles;
